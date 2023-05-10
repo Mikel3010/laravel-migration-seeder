7fa2b7acb1225f9fc38b4e26eb91b2f4b43e426c
Mikel3010 <mikelgremi30@gmail.com>
2023-05-10 19:40:15 +0200

    migration

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use App\Models\Train;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index(){
+        $trains = Train::where('departure_time','>=', now('Europe/Rome'))->get();
+        return view('home',compact('trains'));
+    }
+}
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,13 @@
          @vite('resources/js/app.js')
     </head>
     <body>
+        <h1>Lista Treni</h1>
         <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+            <ul>
+                @foreach ($trains as $train)
+                <li>Codice treno:{{$train->train_code}} <br> -Partenza:{{$train->departure_time}} <br> -Arrivo:{{$train->arrivale_time}}</li>  
+                @endforeach
+            </ul>
         </div>
 
     </body>
